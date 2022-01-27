094446a52dd91f201e3299d3488f6cb5a68380c4
BFlameSwift <19373732@buaa.edu.cn>
2022-01-27 23:23:03 +0800

    :mag: chore: add some db msg

--- model/judge.go
+++ model/judge.go
@@ -2,12 +2,14 @@ package model
 
 // 比赛
 type Competition struct {
-	ID   uint64 `gorm:"primary_key; not null;" json:"id"`
-	Name string `gorm:"size:32; not null; unique" json:"name"`
+	ID    uint64 `gorm:"primary_key; not null;" json:"id"`
+	OrgID uint64 `gorm:"not null;" json:"org_id"` // 所属比赛ID
+	Name  string `gorm:"size:32; not null; unique" json:"name"`
 }
 
 // 题目
 type Problem struct {
-	ID   uint64 `gorm:"primary_key; not null;" json:"id"`
-	Name string `gorm:"size:32; not null; unique" json:"name"`
+	ID    uint64 `gorm:"primary_key; not null;" json:"id"`
+	Title string `gorm:"size:32; not null;" json:"title"`
+	Name  string `gorm:"size:32; not null; unique" json:"name"`
 }
--- model/social.go
+++ model/social.go
@@ -1,18 +1,25 @@
 package model
 
+import "time"
+
 // 用户
 type User struct {
-	ID       uint64 `gorm:"primary_key; not null;" json:"id"`
-	Name     string `gorm:"size:32; not null; unique" json:"name"`
-	Email    string `gorm:"size:32; not null; unique;" json:"email"`
-	Password string `gorm:"size:32; not null;" json:"password"`
-	Avatar   string `gorm:"size:256;" json:"avatar"`
-	Profile  string `gorm:"size:255;" json:"profile"`
+	ID       uint64    `gorm:"primary_key; not null;" json:"id"`
+	Name     string    `gorm:"size:32; not null; unique" json:"name"`
+	Email    string    `gorm:"size:32; not null; unique;" json:"email"`
+	Password string    `gorm:"size:32; not null;" json:"password"`
+	Avatar   string    `gorm:"size:256;" json:"avatar"`
+	Profile  string    `gorm:"size:255;" json:"profile"`
+	RegTime  time.Time `gorm:"datetime" json:"reg_time"`
+	UserType uint64    `gorm:"default:0;" json:"user_type"` // 0为普通用户，1为至少为某一组织的管理员
 }
 
 // 组织
 type Organization struct {
-	ID      uint64 `gorm:"primary_key; not null;" json:"id"`
-	Name    string `gorm:"size:32; not null; unique" json:"name"`
-	Profile string `gorm:"size:255;" json:"profile"`
+	ID          uint64    `gorm:"primary_key; not null;" json:"id"`
+	Name        string    `gorm:"size:32; not null; unique" json:"name"`
+	Profile     string    `gorm:"size:255;" json:"profile"`
+	CreatorID   uint64    `gorm:"not null;" json:"creator_id"` // 创建者ID
+	CreatorName string    `gorm:"size:32;not null;" json:"creator_name"`
+	CreatedTime time.Time `gorm:"datetime;" json:"created_time"`
 }
